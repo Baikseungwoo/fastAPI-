8a65f0f9857a52082da58ddf10ffb6450850ec28
Jin <koreanjinjin2@gmail.com>
2026-04-03 19:25:32 +0900

    main.py수정

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,27 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.concurrency import asynccontextmanager
-from app.routers import user, board
 from app.db.database import Base, async_engine
 from app.middleware.token_refresh import RefreshTokenMiddleware
-from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=".env")
-
-# DB 연결 후 테이블 생성
-# 종료 시 DB 연결 해제
 @asynccontextmanager
 async def lifespan(app:FastAPI):
+    # 앱 시작 시 테이블 생성
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
+    # 앱 종료 시 리소스 정리
     await async_engine.dispose()
 
 app=FastAPI(lifespan=lifespan)
 
-app.add_middleware(RefreshTokenMiddleware)
+
+# app.include_router(경로.router) 추후 연결
+
+
+
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,5 +30,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# app.include_router(경로.router) 연결해야 함
+app.add_middleware(RefreshTokenMiddleware)
