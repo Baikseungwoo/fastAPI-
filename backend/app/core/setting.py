703b50f0ffe5ec90ba79fe3282ccf75343eb3a76
Baikseungwoo <swbaik01@naver.com>
2026-04-05 18:00:49 +0900

    fix error in app-core-setting.py for base setting

--- backend/app/core/setting.py
+++ backend/app/core/setting.py
@@ -1,12 +1,18 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 # 외부 값 없을 경우 사용할 기본값
 class Setting(BaseSettings):
-    db_user:str="root"
-    db_password:str="1234"
-    db_host:str="localhost"
-    db_port:str="3306"
-    db_name:str="board"
+    db_user:str=Field(..., alias="DB_USER")
+    db_password:str=Field(..., alias="DB_PASSWORD")
+    db_host:str=Field("localhost", alias="DB_HOST")
+    db_port:str=Field("3306", alias="DB_PORT")
+    db_name:str=Field(..., alias="DB_NAME")
+    secret_key:str=Field(..., alias="SECRET_KEY")
+    jwt_algorithm:str=Field("HS256", alias="JWT_ALGORITHM")
+    access_token_expire_seconds:int=Field(900, alias="ACCESS_TOKEN_EXPIRE")
+    refresh_token_expire_seconds:int=Field(604800, alias="REFRESH_TOKEN_EXPIRE")
+
 
 # 설정 관리: 환경변수에서 값을 자동으로 불러올 수 있는 클래스
     class Config:
