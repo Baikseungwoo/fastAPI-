7ca76e454fe4b5c24d6292acb754654bb37024a3
Baikseungwoo <swbaik01@naver.com>
2026-04-10 18:09:09 +0900

    fix

--- backend/app/db/crud/__init__.py
+++ backend/app/db/crud/__init__.py
@@ -1,4 +1,5 @@
 from .cart import CartCrud
 from .purchase import PurchaseCrud
 from .user import UserCrud
-from .factory import Factu
+from .factory import FactoryCrud
+from .product import ProductCrud
